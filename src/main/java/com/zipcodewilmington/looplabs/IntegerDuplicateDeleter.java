444b2f03df13894d76f89ec6820819e097935eff
surabhi <surabhi.das@outlook.com>
2023-07-27 17:56:52 -0400

    intDupeDelete

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,46 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        return new Integer[0];
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        String noDupe = "";
+
+        for (int i =0;i<array.length;i++){
+            //for comparing in the second for loop
+            Integer integer = array[i];
+            Integer counter =0;
+
+            for (int j=0;j<array.length;j++){
+                if (integer.equals(array[j])){
+                    //we are keeping track of this counter bc will compare it to the given var exactNumOfDuplications
+                    counter++;
+
+                }
+            }
+            //now we wana pull out all the values that dont equal the given parameter
+            if(!(counter==exactNumberOfDuplications)){
+                noDupe += array[i] +" ";
+            }
+
+        }
+        //split at  spaces
+        String [] stringResult = noDupe.split(" " );
+        Integer [] results = new Integer[stringResult.length];
+
+        for (int i =0;i<results.length;i++){
+            //so now we are getting the Int value of stringResult
+            results[i] =Integer.valueOf(stringResult[i]);
+        }
+        return results;
+                //new Integer[0];
+    }
 }
