3041d41e17f5f30e4df32f6bb13c88b024846c04
surabhi <surabhi.das@outlook.com>
2023-07-27 22:21:06 -0400

    done Int and Str

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -11,7 +11,45 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
 
     @Override
     public Integer[] removeDuplicates(int maxNumberOfDuplications) {
-        return new Integer[0];
+        String noDupe = "";
+
+        for (int i =0;i<array.length;i++){
+            //for comparing in the second for loop
+            Integer integer = array[i];
+            Integer counter =0;
+
+            for (int j=0;j<array.length;j++){
+                if (integer.equals(array[j])){
+                    //we are keeping track of this counter bc will compare it to the given var exactNumOfDuplications
+                    counter++;
+
+                }
+            }
+            //now we wana pull out all the values that dont equal the given parameter
+            //given 2 for instance, if theres anything that is in the array more than twice it
+            //gets taken out
+            if(!(counter>=maxNumberOfDuplications)){
+                //if the given number is 0 or 1 the counter would never be greater
+                //and array would be empty
+                noDupe += array[i] +" ";
+            }
+
+        }
+        //split at  spaces
+        String [] stringResult = noDupe.split(" " );
+        Integer [] results = new Integer[stringResult.length];
+        //incase array is empty
+        if(noDupe.equals("")) {
+            //return an empty array
+            return new Integer[0];
+        } else{
+
+        for (int i =0;i<results.length;i++){
+            //so now we are getting the Int value of stringResult
+            results[i] =Integer.valueOf(stringResult[i]);
+        }
+            }
+        return results;
     }
 
     @Override
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -12,11 +12,62 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
 
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
+        String noDupe = "";
+
+        for (int i =0;i<array.length;i++){
+            //for comparing in the second for loop
+            String str = array[i];
+            Integer counter =0;
+
+            for (int j=0;j<array.length;j++){
+                if (str.equals(array[j])){
+                    //we are keeping track of this counter bc will compare it to the given var exactNumOfDuplications
+                    counter++;
+
+                }
+            }
+
+            if(!(counter>=maxNumberOfDuplications)){
+                //dont want all the strings to joined together into one word
+                noDupe += array[i]+ " ";
+            }
+
+        }
+        if(noDupe.length()==0){
+            return new String[0];
+        }
+
+        String [] result = noDupe.split(" ");
+        return result;
     }
+
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
+        String noDupe = "";
+
+        for (int i =0;i<array.length;i++){
+            //for comparing in the second for loop
+            String str = array[i];
+            Integer counter =0;
+
+            for (int j=0;j<array.length;j++){
+                if (str.equals(array[j])){
+                    //we are keeping track of this counter bc will compare it to the given var exactNumOfDuplications
+                    counter++;
+
+                }
+            }
+
+            if(!(counter==exactNumberOfDuplications)){
+                //dont want all the strings to joined together into one word
+                noDupe += array[i]+ " ";
+            }
+
+        }
+
+        String [] result = noDupe.split(" ");
+
+        return result;
     }
 }
